4275b47456a421e08aba3a74266b7ce44f2bffdf
Beast <95392136+KBailey-95@users.noreply.github.com>
2022-01-31 02:31:01 -0500

    Work to add functionality to the quiz. Not fully successful.

--- script.js
+++ script.js
@@ -1,27 +1,73 @@
-const startButton = document.getElementsByID("start-btn");
-const questionsContainer = document.getElementById("question-container");
-const shuffledQuestions, currentQuestionIndex
+//grab relevant DOM elements
+var startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainer = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+var shuffledQuestions = "";
+var currentQuestion = "";
+const answerButtonsEl = document.getElementById("answer-buttons");
 
 
 
 
-
+//make start and next buttpon
 startButton.addEventListener("click", startQuiz);
+nextButton.addEventListener("click", () => {
+    currentQuestion++;
+    nextQuestion();
+})
 
+
+//set what happens upon game start
 function startQuiz(){
+console.log("hello");
+//hide start button when game begins
 startButton.classList.add("hide");
-questionsContainer.classList.remove("hide");
+//shuffle questions every time game is started 
+shuffledQuestions = questions.sort(() => Math.random() - .5);
+currentQuestion = 0;
+questionContainer.classList.remove("hide");
+nextButton.classList.remove("hide");
 nextQuestion();
 }
 
+//move to the next question
 function nextQuestion(){
+    showQuestion(shuffledQuestions[currentQuestion]);
 
 }
 
-function chooseAnswer() {
+function showQuestion(question) {
+   resetState();
+    questionElement.innerText = question.question;
+    question.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerText = answer.text;
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", chooseAnswer);
+        answerButtonsEl.appendChild("button");
+
+});
+
+//reset state of the quiz after a question is answered 
+function resetState(){
+    nextButton.classList.add("hide");
+    while (answerButtonsEl.firstChild) {
+        answerButtonsEl.removeChild;
+        (answerButtonsEl.firstChild);
+    }
+}
+
+function chooseAnswer(e) {
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
 
 }
 
+//how the correct answer is actually selected
 const questions = [
     {
         question: "Fill in the blank. H_ML",
@@ -32,4 +78,4 @@ const questions = [
             {text: "K", correct: false }
         ]
     }
-]
+]}
